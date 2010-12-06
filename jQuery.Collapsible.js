1cc18839163eddfbef840048580ad45755fc1500
John Snyder <juven14@gmail.com>
2010-12-06 12:56:12 -0500

    Added custom animation for open and close, updated docs, renamed files to lowercase, minified latest

--- jQuery.Collapsible.js
+++ jQuery.Collapsible.js
@@ -6,148 +6,168 @@
  * 
  * Copyright (c) 2010 John Snyder (snyderplace.com)
  * @license http://www.snyderplace.com/collapsible/license.txt New BSD
+ * @version 1.1
  */
 (function($) {
 	$.fn.collapsible = function(cmd, arg) {
+		//address command requests
 		if (typeof cmd == 'string') {
 			return $.fn.collapsible.dispatcher[cmd](this, arg);
 		}
-
+		
+		//return the command dispatcher
 		return $.fn.collapsible.dispatcher['_create'](this, cmd);
 	};
 
-	$.fn.collapsible.dispatcher = {
+	//create the command dispatcher
+	$.fn.collapsible.dispatcher = {		
+		//initialized with options
 		_create : function(obj, arg) {
 			createCollapsible(obj, arg);
 		},
+		//toggle the element's display
 		toggle: function(obj) {
 			toggle(obj, loadOpts(obj));
 			return obj;
 		},
+		//show the element
 		open: function(obj) {
 			open(obj, loadOpts(obj));
 			return obj;
 		},
+		//hide the element
 		close: function(obj) {
 			close(obj, loadOpts(obj));
 			return obj;
 		},
+		//check if the element is closed
 		collapsed: function(obj) {
 			return collapsed(obj, loadOpts(obj));
-		},
+		}
 	};
 
+	//create the initial collapsible
 	function createCollapsible(obj, options) {
-		// build main options before element iteration
+		//build main options before element iteration
 		var opts = $.extend({}, $.fn.collapsible.defaults, options);
-		// store any opened default values to set cookie later
+		
+		//store any opened default values to set cookie later
 		var opened = new Array();
 		
-		// iterate and reformat each matched element
+		//iterate each matched object, bind, and open/close
 		obj.each(function() {
 			var $this = $(this);
 			saveOpts($this, opts);
 			
-			//lets bind it to mouseenter
+			//bind it to the event
 			if (opts.bind == 'mouseenter') {
 				$this.bind('mouseenter', function(e) {
-					e.preventDefault(); //stop links from firing
-					toggle($this, opts); //toggle open/close
+					e.preventDefault(); 
+					toggle($this, opts);
 				});
 			}
 			
-			//lets bind it to mouseover
+			//bind it to the event
 			if (opts.bind == 'mouseover') {
 				$this.bind('mouseover',function(e) {
-					e.preventDefault(); //stop links from firing
-					toggle($this, opts); //toggle open/close
+					e.preventDefault(); 
+					toggle($this, opts); 
 				});
 			}
 			
-			//lets bind it to click
+			//bind it to the event
 			if (opts.bind == 'click') {
 				$this.bind('click', function(e) {
-					e.preventDefault(); //stop links from firing
-					toggle($this, opts); //toggle open/close
+					e.preventDefault();
+					toggle($this, opts);
 				});
 			}
 			
-			//lets bind it to dblclick
+			//bind it to the event
 			if (opts.bind == 'dblclick') {
 				$this.bind('dblclick', function(e) {
-					e.preventDefault(); //stop links from firing
-					toggle($this, opts); //toggle open/close
+					e.preventDefault();
+					toggle($this, opts);
 				});
 			}
 			
-			//lets initialize the collapsibles
+			//initialize the collapsibles
 			//get the id for this element
 			id = $this.attr('id');
-			//do we use cookies
-			if (!useCookies(opts)) { //no just open the defaults
+			
+			//if not using cookies, open defauls
+			if (!useCookies(opts)) {
 				//is this collapsible in the default open array?
 				dOpenIndex = inDefaultOpen(id, opts);
-				if (dOpenIndex === false) { //no, close it
-					$this.addClass(opts.cssClose); //append the close class
+				
+				//close it if not defaulted to open
+				if (dOpenIndex === false) {
+					$this.addClass(opts.cssClose);
 					$this.next().hide(); 
-				} else { //yes open it
-					$this.addClass(opts.cssOpen); //append the open class
+				} else { //its a default open, open it
+					$this.addClass(opts.cssOpen);
 					$this.next().show();
 					opened.push(id);
 				}
-			} else { //we can use cookies, let do so
+			} else { //can use cookies, use them now
 				//has a cookie been set, this overrides default open
 				if (issetCookie(opts)) {
 					cookieIndex = inCookie(id, opts);
 					if (cookieIndex === false) { 
-						$this.addClass(opts.cssClose);//append the close class
+						$this.addClass(opts.cssClose);
 						$this.next().hide();
 					} else {
-						$this.addClass(opts.cssOpen);//append the open class
+						$this.addClass(opts.cssOpen);
 						$this.next().show();
 						opened.push(id);
 					}
 				} else { //a cookie hasn't been set open defaults, add them to opened array
 					dOpenIndex = inDefaultOpen(id, opts);
 					if (dOpenIndex === false) {
-						$this.addClass(opts.cssClose);//append the close class
+						$this.addClass(opts.cssClose);
 						$this.next().hide();
 					} else {
-						$this.addClass(opts.cssOpen);//append the open class
+						$this.addClass(opts.cssOpen);
 						$this.next().show();
 						opened.push(id);
 					}
 				}
 			}
 		});
-		//now that the loop is done, lets set the cookie
+		
+		//now that the loop is done, set the cookie
 		if (opened.length > 0 && useCookies(opts)) {
 			setCookie(opened.toString(), opts);
-		} else {
+		} else { //there are none open, set cookie
 			setCookie('', opts);
 		}
+		
 		return obj;
 	};
-	// Load opts from object
+	
+	//load opts from object
 	function loadOpts($this) {
 		return $this.data('collapsible-opts');
 	}
-	// Save opts into object
+	
+	//save opts into object
 	function saveOpts($this, opts) {
 		return $this.data('collapsible-opts', opts);
 	}
-	//Returns true if object is opened
-	function collapsed($this, opts)
-	{
+	
+	//returns true if object is opened
+	function collapsed($this, opts) {
 		return $this.hasClass(opts.cssClose);
 	}
-	// Hides a collapsible
+	
+	//hides a collapsible
 	function close($this, opts) {
-		//give the proper class to the heading
+		//give the proper class to the linked element
 		$this.addClass(opts.cssClose).removeClass(opts.cssOpen);
-		//still here, close an open item
-		$this.next().slideUp(opts.speed); //no animation as its a page load initalization
-		//return false; //just incase its a link, lets not let it fire off
+		
+		//close the element
+		opts.animateOpen($this, opts);
+		
 		//do cookies if plugin available
 		if (useCookies(opts)) {
 			// split the cookieOpen string by ","
@@ -155,10 +175,15 @@
 			unsetCookieId(id, opts);
 		}
 	}
-	// Opens a collapsible
+	
+	//opens a collapsible
 	function open($this, opts) {
+		//give the proper class to the linked element
 		$this.removeClass(opts.cssClose).addClass(opts.cssOpen);
-		$this.next().slideDown(opts.speed);
+		
+		//open the element
+		opts.animateClose($this, opts);
+		
 		//do cookies if plugin available
 		if (useCookies(opts)) {
 			// split the cookieOpen string by ","
@@ -166,57 +191,68 @@
 			appendCookie(id, opts);
 		}
 	}
-	//Toggle a collapsible on an event
-	function toggle($this, opts)
-	{
-		// open a closed item
+	
+	//toggle a collapsible on an event
+	function toggle($this, opts) {
 		if (collapsed($this, opts)) {
+			//open a closed element
 			open($this, opts);
 		} else {
+			//close an open element
 			close($this, opts);
 		}
+		
 		return false;
 	}
-	//do we use cookies
-	function useCookies(opts)
-	{
+	
+	//use cookies?
+	function useCookies(opts) {
 		//return false if cookie plugin not present or if a cookie name is not provided
 		if (!$.cookie || opts.cookieName == '') {
 			return false;
 		}
+		
 		//we can use cookies
 		return true;
-	}	
+	}
+	
 	//append a collapsible to the cookie
-	function appendCookie(value, opts)
-	{
+	function appendCookie(value, opts) {
 		//check if cookie plugin available and cookiename is set
 		if (!useCookies(opts)) {
 			return false;
 		}
+		
 		//does a cookie already exist
 		if (!issetCookie(opts)) { 
 			//no lets set one
 			setCookie(value, opts);
 			return true;
 		}
+		
 		//cookie already exists, is this collapsible already set?
 		if (inCookie(value, opts)) { //yes, quit here
 			return true;
 		}
-		//still here, we have a cookie and need to append our collapsible
+		
 		//get the cookie
 		cookie = $.cookie(opts.cookieName);
+		
 		//unescape it
 		cookie = unescape(cookie);
+		
 		//turn it into an array
 		cookieArray = cookie.split(',');
+		
 		//add it to list
 		cookieArray.push(value);
+		
 		//save it
 		setCookie(cookieArray.toString(), opts);
+		
 		return true;	
 	}
+	
 	//unset a collapsible from the cookie
 	function unsetCookieId(value, opts)
 	{
@@ -224,25 +260,33 @@
 		if (!useCookies(opts)) {
 			return false;
 		}
+		
 		//if its not there we don't need to remove from it
 		if (!issetCookie(opts)) { //quit here, don't have a cookie 
 			return true;
 		}
+		
 		//we have a cookie, is this collapsible in it
 		cookieIndex = inCookie(value, opts);
 		if (cookieIndex === false) { //not in the cookie quit here
 			return true;
 		}
+		
 		//still here get the cookie
 		cookie = $.cookie(opts.cookieName);
+		
 		//unescape it
 		cookie = unescape(cookie);
+		
 		//turn it into an array
 		cookieArray = cookie.split(',');
+		
 		//lets pop it out of the array
 		cookieArray.splice(cookieIndex, 1);
+		
 		setCookie(cookieArray.toString(), opts);
 	}
+	
 	//set a cookie
 	function setCookie(value, opts)
 	{
@@ -250,9 +294,11 @@
 		if (!useCookies(opts)) { //no, quit here
 			return false;
 		}
+		
 		//cookie plugin is available, lets set the cookie
 		$.cookie(opts.cookieName, value, opts.cookieOptions);
 	}
+	
 	//check if a collapsible is in the cookie
 	function inCookie(value, opts)
 	{
@@ -260,23 +306,29 @@
 		if (!useCookies(opts)) {
 			return false;
 		}
+		
 		//if its not there we don't need to remove from it
 		if (!issetCookie(opts)) { //quit here, don't have a cookie 
 			return false;
 		}
+		
 		//unescape it
 		cookie = unescape($.cookie(opts.cookieName));
+		
 		//turn it into an array
 		cookieArray = cookie.split(',');
+		
 		//get the index of the collapsible if in the cookie array
 		cookieIndex = $.inArray(value, cookieArray);
+		
 		//is this value in the cookie arrray
 		if (cookieIndex == -1) { //no, quit here
 			return false;
 		}
-		//yes, return the index
+		
 		return cookieIndex;
 	}
+	
 	//check if a cookie is set
 	function issetCookie(opts)
 	{
@@ -284,36 +336,41 @@
 		if (!useCookies(opts)) { //no, quit here
 			return false;
 		}
+		
 		//is the cookie set
 		if ($.cookie(opts.cookieName) == null) { //no, quit here
 			return false;
 		}
-		//the cookie is set and we can use the cookie plugin
+		
 		return true;
 	}
+	
 	//check if a collapsible is in the list of collapsibles to be opened by default
 	function inDefaultOpen(id, opts)
 	{
 		//get the array of open collapsibles
 		defaultOpen = getDefaultOpen(opts);
+		
 		//is it in the default open array
 		index = $.inArray(id, defaultOpen);
 		if (index == -1) { //nope, quit here
 			return false;
 		}
-		//it is a default open item, return its index
+		
 		return index;
 	}
+	
 	//get the default open collapsibles and return array
 	function getDefaultOpen(opts)
 	{
 		//initialize an empty array
 		defaultOpen = new Array();
+		
 		//if there is a list, lets split it into an array
 		if (opts.defaultOpen != '') {
 			defaultOpen = opts.defaultOpen.split(',');
 		}
-		//return the default open array
+		
 		return defaultOpen;
 	}
 	
@@ -329,7 +386,13 @@
 			secure: ''
 		},
 		defaultOpen: '', //comma seperated list of header ids that you want opened by default
-		speed: 300, //speed of the slide effect
-		bind: 'click' //event to bind to, supports click, dblclick, mouseover and mouseenter
+		speed: 'slow', //speed of the slide effect
+		bind: 'click', //event to bind to, supports click, dblclick, mouseover and mouseenter
+		animateOpen: function (elem, opts) { //replace the standard slideUp with custom function
+			elem.next().slideUp(opts.speed);
+		},
+		animateClose: function (elem, opts) { //replace the standard slideDown with custom function
+			elem.next().slideDown(opts.speed);
+		}
 	};
 })(jQuery);
